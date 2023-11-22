456194e4040356bc09a09f0e97f540a458ce72b7
Antonio <taminto.a@gmail.com>
2023-11-22 11:39:32 +0100

    commit

---
+++ src/Forma.java
@@ -0,0 +1,4 @@
+public abstract class Forma {
+
+    public abstract void calcolaArea(int b,int a);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Rettangolo rettangolo = new Rettangolo();
+        Triangolo triangolo = new Triangolo();
+
+        triangolo.calcolaArea(6,8);
+        rettangolo.calcolaArea(6,7);
     }
 }
